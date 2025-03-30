c9392e694dcdcf8980faf38b853875b6b4de8e70
Tiffani A. Rengifo Mori <tiffanirengifo23@gmail.com>
2025-03-29 21:15:31 -0500

    updated

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 import os
 from dotenv import load_dotenv
+import pymysql
 
-load_dotenv()
+pymysql.install_as_MySQLdb()
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY','mi_clave_secreta')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+print(f"DATABASE_URL: {os.getenv('DATABASE_URL')}")
